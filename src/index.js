81108908e05eddcad631fcc908dcebb46ef5e666
Ash Dan <Arvolley01@gmail.com>
2022-07-06 14:29:47 -0400

    commit tree c39b05016da2d9e3d6e29a349122aef20364b4be

--- src/index.js
+++ src/index.js
@@ -1,72 +1,65 @@
-// write your code here
-//1. fetch 
-function fetchRamen(){
-    fetch('http://localhost:4000/ramens')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        renderRamens(data)
+let nextID = 0;
+
+fetch('http://localhost:3000/ramens')
+    .then(res => res.json())
+    .then((data) => {
+        nextID = data.length
     })
-}
 
-fetchRamen()
-
-//2. create global variables of html tags 
-
-let menu = document.getElementById('ramen-menu')
-
-let image = document.createElement('img')
-
-//these are from the details section
-let ramenImage = document.querySelector('.detail-image')
-let ramenName = document.querySelector('.name')
-let ramenRestaurant = document.querySelector('.restaurant')
-let ramenRating = document.getElementById('rating-display')
-let ramenComment = document.getElementById('comment-display')
-
-//these are the variables from the form tag
-let ramenForm = document.getElementById('new-ramen')
-let newName = document.getElementById('new-name')
-let newRestaurant = document.getElementById('new-restaurant')
-let newImage = document.getElementById('new-image')
-let newComment = document.getElementById('new-comment')
-let newRating = document.getElementById('new-rating')
-
-const renderRamens = (data) => {
-    console.log(data, " From the render function")
-    for (const item of data){
-        console.log(item)
-        let image = document.createElement('img')
-        image.src =  item.image
-        menu.append(image)
-
-        image.addEventListener('click', function(){
-            ramenImage.src = item.image
-            ramenName.innerHTML =`${item.name}`
-            ramenRestaurant.innerHTML = `${item.restaurant}`
-            ramenRating.innerHTML = item.rating
-            ramenComment.innerHTML =   item.comment
-
+const displayRamen = (ind) => {
+    const viewing = document.getElementById('ramen-detail')
+    fetch(`http://localhost:3000/ramens/${ind+1}`)
+        .then(res => res.json())
+        .then((data) => {
+            viewing.getElementsByClassName('detail-image')[0].src = data.image
+            viewing.getElementsByClassName('name')[0].textContent = data.name
+            viewing.getElementsByClassName('restaurant')[0].textContent = data.restaurant
+            document.getElementById('rating-display').textContent = data.rating
+            document.getElementById('comment-display').textContent = data.comment
         })
-    }
 }
 
-const addNewRamen = () =>{
-    ramenForm.addEventListener('submit', function(e){
-        e.preventDefault()
-        ramenForm.reset()
-        console.log('Submitted')
-        console.log(newName.value)
-        let newRamen = [{"name": newName.value, "restaurant": newRestaurant.value, "image": newImage.value, "rating": newRating.value, "comment": newComment.value}]
-        console.log(newRamen, "   This is the new Object")
-        renderRamens(newRamen)
+const renderRamen = () => {
+    fetch('http://localhost:3000/ramens')
+    .then(res => res.json())
+    .then((data) => {
+        const menu = document.getElementById('ramen-menu')
+        menu.innerHTML = ''
+        data.forEach((element, index) => {
+            const im = document.createElement('img')
+            im.src = element.image
+            im.addEventListener('click', () => {
+                displayRamen(index)
+            })
+            menu.append(im)
+        });
     })
 }
 
-addNewRamen()
+form = document.getElementById('new-ramen')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    fetch('http://localhost:3000/ramens', {
+        method: 'POST', 
+        mode: 'cors',
+        cache: 'no-cache', 
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow', // manual, *follow, error
+        body: JSON.stringify({
+            "id": ++nextID,
+            "name": document.getElementById('new-name').value,
+            "restaurant": document.getElementById('new-restaurant').value,
+            "image": document.getElementById('new-image').value,
+            "rating": document.getElementById('new-rating').value,
+            "comment": document.getElementById('new-comment').value
+        })
+    }).catch((error) => console.log(error))
+    renderRamen()
+})
 
 
 
-
-
-
+renderRamen()
